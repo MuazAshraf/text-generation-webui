d171928a8715cf498b190f213d4573195d072921
Muaz <98591478+MuazAshraf@users.noreply.github.com>
2023-08-03 20:43:55 +0500

    Update minimal.py

--- minimal.py
+++ minimal.py
@@ -3,5 +3,6 @@ import gradio as gr
 def greet(name):
     return f"Hello {name}!"
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
+iface = gr.Interface(fn=greet, inputs="text", outputs="text").launch(share=True)
 iface.launch(server_name='0.0.0.0', server_port=7860)
+
